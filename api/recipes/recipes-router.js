53938109b05275ff2c8801dcabcf55e7c6e0d9e2
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-17 18:33:37 -0600

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,14 +1,21 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
 
-router.use('*', (req,res)=>{
-  res.json({ api: 'working'})
+
+router.get('/:recipe_id', (req,res,next)=>{
+  Recipe.getRecipeById(req.params.recipe_id)
+    .then(resource => {
+      res.status(200).json(resource)
+    })
+    .catch(next)
 })
+
 
 router.use((err, req, res, next)=>{ // eslint-disable-line
   res.status(500).json({
     cstmMsg: 'something went wrong ;(',
-    msg: err.msg,
-    stk: err.stk
+    message: err.message,
+    stack: err.stack
   })
 })
 
